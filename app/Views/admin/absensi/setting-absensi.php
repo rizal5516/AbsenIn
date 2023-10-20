351f529f68012c506416bc52859dd13c0892d6df
rizal5516 <rizalqowi17@gmail.com>
2023-10-20 20:44:44 +0700

    map-setting-absen

--- app/Views/admin/absensi/setting-absensi.php
+++ app/Views/admin/absensi/setting-absensi.php
@@ -68,7 +68,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                 <div class="iq-card">
                     <div class="iq-card-header d-flex justify-content-between">
                         <div class="iq-header-title">
