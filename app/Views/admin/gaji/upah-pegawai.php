35b67de4ff7a988911e4a61bb7d1fd8e6382f09d
rizal5516 <rizalqowi17@gmail.com>
2023-12-09 00:54:50 +0700

    onprogress-viewupah

--- app/Views/admin/gaji/upah-pegawai.php
+++ app/Views/admin/gaji/upah-pegawai.php
@@ -10,7 +10,7 @@
         <div class="iq-card">
             <div class="iq-card-header d-flex justify-content-between">
                 <div class="iq-header-title">
-                    <h4 class="card-title">Detail Gaji</h4>
+                    <h4 class="card-title">Detail Gaji <?= $pegawai->nama_pegawai ?> Bulan <?= date('F Y', strtotime('-1 month', strtotime($gaji['bulan']))); ?></h4>
                 </div>
                 <span class="table-add float-right mb-3 m-3 ">
                     <a href="<?= base_url('gaji/dataPenggajian'); ?>" class="btn btn-sm iq-bg-danger"><i class="icon-arrow_back"><span class="pl-1">Back
@@ -19,170 +19,35 @@
                 </span>
             </div>
         </div>
-        <!-- DETAIL ABSEN -->
-        <div class="row ">
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Nama</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= $pegawai->nama_pegawai; ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">NIK</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= $pegawai->nip; ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Jabatan</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= $pegawai->nama_jabatan; ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Gaji Pokok</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['gaji_pokok'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Tunjangan Pegawai</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['tunjangan'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Upah per jam</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['upah'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Denda Absen</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['denda'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Bonus Siswa</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['bonus_siswa'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Bonus Absen</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['bonus_absen'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Bulan</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= date('F Y', strtotime('-1 month', strtotime($gaji['bulan']))); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Jumlah Jam Kerja</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= $gaji['jumlah_jam_kerja']; ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Jumlah Denda</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= $gaji['jumlah_denda']; ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Jumlah Bonus Siswa</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= $gaji['jumlah_bonus_siswa']; ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Jumlah Bonus Absen</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= $gaji['jumlah_bonus_absen']; ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Total Upah</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['total_upah'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Total Denda</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['total_denda'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Total Bonus Siswa</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['total_bonus_siswa'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Total Bonus Absen</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-secondary">: <?= "Rp. " . number_format($gaji['total_bonus_absen'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <h6 class="text-secondary">Gaji Bersih</h6>
-                            </div>
-                            <div class="col-lg-7">
-                                <h6 class="text-primary">: <?= "Rp. " . number_format($gaji['gaji_bersih'], 0, ',', '.'); ?></h6>
-                            </div>
-                        </div>
+
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <div class="iq-card">
+                    <div class="iq-card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><b>Nama:</b> <?= $pegawai->nama_pegawai; ?></li>
+                            <li class="list-group-item"><b>NIK:</b> <?= $pegawai->nip; ?></li>
+                            <li class="list-group-item"><b>Jabatan:</b> <?= $pegawai->nama_jabatan; ?></li>
+                            <li class="list-group-item"><b>Gaji Pokok:</b> <?= "Rp. " . number_format($gaji['gaji_pokok'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Tunjangan Pegawai:</b> <?= "Rp. " . number_format($gaji['tunjangan'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Upah per jam:</b> <?= "Rp. " . number_format($gaji['upah'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Denda Absen:</b> <?= "Rp. " . number_format($gaji['denda'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Bonus Siswa:</b> <?= "Rp. " . number_format($gaji['bonus_siswa'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Bonus Absen:</b> <?= "Rp. " . number_format($gaji['bonus_absen'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Bulan:</b> <?= date('F Y', strtotime('-1 month', strtotime($gaji['bulan']))); ?></li>
+                            <li class="list-group-item"><b>Jumlah Jam Kerja:</b> <?= $gaji['jumlah_jam_kerja']; ?></li>
+                            <li class="list-group-item"><b>Jumlah Bonus Siswa:</b> <?= $gaji['jumlah_bonus_siswa']; ?></li>
+                            <li class="list-group-item"><b>Jumlah Bonus Absen:</b> <?= $gaji['jumlah_bonus_absen']; ?></li>
+                            <li class="list-group-item"><b>Total Upah:</b> <?= "Rp. " . number_format($gaji['total_upah'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Total Denda:</b> <?= "Rp. " . number_format($gaji['total_denda'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Total Bonus Siswa:</b> <?= "Rp. " . number_format($gaji['total_bonus_siswa'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Total Bonus Absen:</b> <?= "Rp. " . number_format($gaji['total_bonus_absen'], 0, ',', '.'); ?></li>
+                            <li class="list-group-item"><b>Gaji Bersih:</b> <?= "Rp. " . number_format($gaji['gaji_bersih'], 0, ',', '.'); ?></li>
+                        </ul>
                     </div>
                 </div>
-
             </div>
         </div>
-        <!-- end DETAIL ABSEN -->
-
     </div>
     <!-- Content wrapper end -->
 
